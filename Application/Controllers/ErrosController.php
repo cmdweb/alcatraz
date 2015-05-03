269e75c958b6fa8397abf7091ca992630db3bfc9
Gabriel Malaquias <gemalaquias@gmail.com>
2015-05-03 01:23:03 -0300

    Ajustes Erros

--- Application/Controllers/ErrosController.php
+++ Application/Controllers/ErrosController.php
@@ -11,6 +11,9 @@ namespace Controllers;
 use Alcatraz\Kernel\Controller;
 
 class ErrosController extends Controller{
+    /**
+     * @AllowAccess
+     */
     public function Erro404(){
         $this->View();
     }
