e19d3554112f44e927d403ebf5d55cc26cb127c7
Gabriel Malaquias <gemalaquias@gmail.com>
2015-05-03 00:16:35 -0300

    Ajustes de layout

--- Application/Controllers/IndexController.php
+++ Application/Controllers/IndexController.php
@@ -2,11 +2,21 @@
 namespace Controllers;
 
 use Alcatraz\Kernel\Controller;
+use Alcatraz\Owl\Generator\ClassGenerator;
+use Alcatraz\Security\Security;
 
 class IndexController extends Controller
 {
-
+    /**
+     * @AllowAccess
+     */
     public function Index()
+    {
+        $this->View();
+    }
+
+
+    public function Index2()
     {
         $this->View();
     }
--- Application/Views/Index/Index.php
+++ Application/Views/Index/Index.php
@@ -1,6 +1,18 @@
 <style>
-    body{
+    html{
         text-align: center;
+        background: #ffffff; /* Old browsers */
+        background: -moz-radial-gradient(center, ellipse cover,  #ffffff 26%, #e0e0e0 100%); /* FF3.6+ */
+        background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(26%,#ffffff), color-stop(100%,#e0e0e0)); /* Chrome,Safari4+ */
+        background: -webkit-radial-gradient(center, ellipse cover,  #ffffff 26%,#e0e0e0 100%); /* Chrome10+,Safari5.1+ */
+        background: -o-radial-gradient(center, ellipse cover,  #ffffff 26%,#e0e0e0 100%); /* Opera 12+ */
+        background: -ms-radial-gradient(center, ellipse cover,  #ffffff 26%,#e0e0e0 100%); /* IE10+ */
+        background: radial-gradient(ellipse at center,  #ffffff 26%,#e0e0e0 100%); /* W3C */
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#e0e0e0',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
     }
 </style>
 
